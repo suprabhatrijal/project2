9667d7711d7c83d22924df00ce83571abeb23d25
Suprabhat Rijal <goodguysman@gmail.com>
2022-11-19 15:35:54 -0500

    removed input prompt

--- project2.s
+++ project2.s
@@ -4,9 +4,10 @@ input_prompt: .asciiz "Input: "
 .text
 main:
 
-# input prompt for testing
-li $v0, 4
-la $a0, input_prompt
+# get input string from the user
+li $v0, 8
+la $a0, input
+li $a1, 1001
 syscall
 
 # exit
