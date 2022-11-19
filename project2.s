33a045e24acb4a455c6bf2de49bde4025fe1ae59
Suprabhat Rijal <goodguysman@gmail.com>
2022-11-19 15:55:51 -0500

    Half the condition for Lower written

--- project2.s
+++ project2.s
@@ -126,12 +126,27 @@ li $t1, 1
 # if char >= 48 and char < 58
 beq $t0, $t1, Number
 
+# character falls in the range  'a' to 'y'
+
+# char < 97
+slti $t0, $s4, 97 
+
+# not (char < 97)  ===> ( char  >= 97)
+nor $t0, $t0, $zero
+
+# char < 123
+slti $t1, $s4, 122
+
+and $t0, $t0, $t1
+
 Number:
 addi $t1, $s4, -48
 li $t2, 30
 mult $t2, $s3
 mflo $s3
 add $s3, $s3, $t1 
+
+j loopCOTD
 
 invalidChar:
 li $v0, -1
