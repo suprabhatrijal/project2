ee368cb75e90bff0f8f313555adc23d843c7065a
Suprabhat Rijal <goodguysman@gmail.com>
2022-11-19 15:41:23 -0500

    Initialized the first pass

--- project2.s
+++ project2.s
@@ -21,5 +21,13 @@ li $v0, 10
 syscall
 
 base_to_decimal:
+# go through the string find the address of start and end of string without spaces or tabs
+
+# initialize the loop
+li $s0, 0 # flag which is true if first valid char has been encountered
+addi $s2, $a0, 0 # Address of the input string
+firstPass:
+lb $s4, 0($s2) # current character
+
 # return to main program
 jr $ra
